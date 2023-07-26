d690e49bde2ebf4199d14e851d234b27275d1b90
Igor1D <43789550+Igor1D@users.noreply.github.com>
2023-07-26 22:05:38 +0200

    new project files

---
+++ code/index.js
@@ -0,0 +1,19 @@
+// async function main() {
+
+//     let results = await fetch(`http://localhost:3000/tasks`);
+//     let data = await results.json();
+//     console.log(data);
+//     console.log(data[0].title);
+
+//     for ( let i=0; i < data.length; i++) {
+//         let newP = document.createElement('p');
+//         newP = data[i].title;
+//         document.body.append(newP);
+//         console.log(data[i].title);
+//     }
+
+
+
+// } 
+
+// main();
