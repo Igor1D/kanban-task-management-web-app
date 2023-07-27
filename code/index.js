df116722052460b250f97a96291051374b45c465
Igor1D <43789550+Igor1D@users.noreply.github.com>
2023-07-27 22:20:41 +0200

    desktop ver

--- code/index.js
+++ code/index.js
@@ -1,19 +1,51 @@
-// async function main() {
+// Btns active
 
-//     let results = await fetch(`http://localhost:3000/tasks`);
-//     let data = await results.json();
-//     console.log(data);
-//     console.log(data[0].title);
+let btns = document.getElementsByClassName("board-btn");
 
-//     for ( let i=0; i < data.length; i++) {
-//         let newP = document.createElement('p');
-//         newP = data[i].title;
-//         document.body.append(newP);
-//         console.log(data[i].title);
-//     }
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    let current = document.getElementsByClassName("active");
+
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+
+    // Add the active class to the current/clicked button
+    this.className += " active";
+  });
+}
 
 
 
-// } 
 
-// main();
+
+
+
+
+async function main() {
+    let selectedBoard = 'Platform Launch' 
+    let results = await fetch(`http://localhost:3000/tasks`);
+    let tasks = await results.json();
+
+    function showTasks() {
+        // go over this on the next class
+        const filtredTasks = tasks.filter(task => task.board == selectedBoard);
+        console.log(filtredTasks);
+
+
+    }
+
+
+    showTasks();
+
+} 
+
+main();
+
+
+// for ( let i=0; i < tasks.length; i++) {
+//     //     let newP = document.createElement('p');
+//     //     newP = tasks[i].title;
+//     //     document.body.append(newP);
+//     //     console.log(tasks[i].board);
+//     // }
