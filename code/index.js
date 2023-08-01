ce936cdd86ff3896768ad16bcba1e206e5f8f8f9
Igor1D <43789550+Igor1D@users.noreply.github.com>
2023-08-01 22:10:12 +0200

    js part inprtogress

--- code/index.js
+++ code/index.js
@@ -4,47 +4,55 @@ let btns = document.getElementsByClassName("board-btn");
 //Tasks
 
 let columnDiv = document.getElementById("column-div");
+let selectedBoard = 'Platform Launch';
 
 
-
-
-
-for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    let current = document.getElementsByClassName("active");
-
-    if (current.length > 0) {
-      current[0].className = current[0].className.replace(" active", "");
-    }
-
-    // Added the active class to the current/clicked button
-    this.className += " active";
-  });
-}
 
 
 
 
 async function main() {
-    let selectedBoard = 'Platform Launch' 
+    
     let results = await fetch(`http://localhost:3000/tasks`);
     let tasks = await results.json();
+    console.log(tasks);
 
+    for (let i = 0; i < btns.length; i++) {
+      btns[i].addEventListener("click", function() {
+        let current = document.getElementsByClassName("active");
     
+        if (current.length > 0) {
+          current[0].className = current[0].className.replace(" active", "");
+        }
+    
+        // Added the active class to the current/clicked button
+        this.className += " active";
+        
+        selectedBoard = btns[i].innerText;
+        showTasks();
+        console.log(selectedBoard);
+      });
+    }
 
     function showTasks() {
-        // go over this on the next class
-        const filteredTasks = tasks.filter(task => task.board == selectedBoard);
+        columnDiv.innerHTML = `<div id="todo-head-div" class ="column-head-div"><span class="dot" id="todo-dot"></span><h4>TODO (4)</h4></div>`;
+        
+        const filteredTasks = tasks.filter((task) => {
+          // console.log(task.board);
+          return task.board == selectedBoard;
+          
+        });
          
 
+
+
         for ( let i=0; i < filteredTasks.length; i++) {
-          let newP = document.createElement('p');
           let taskDiv = document.createElement('div');
-          newP = tasks[i].title;
-          taskDiv.innerHTML = newP;
+          taskDiv.innerText = tasks[i].title;
           taskDiv.classList.add('task-div');
           columnDiv.appendChild(taskDiv);
-          console.log(filteredTasks);
+         
+          
       }
     }
 
