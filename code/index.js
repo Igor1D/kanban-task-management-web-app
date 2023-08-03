d6e402132c8d4b1b21b4a71d6cfe446daa5bedc2
Igor1D <43789550+Igor1D@users.noreply.github.com>
2023-08-03 22:07:55 +0200

    board name is dynamic name

--- code/index.js
+++ code/index.js
@@ -1,10 +1,13 @@
-// Btns active
+// Btns 
 let btns = document.querySelectorAll(".board-btn");
 
 //Tasks
-
 let columnDiv = document.getElementById("column-div");
 let selectedBoard = 'Platform Launch';
+let headerBoardName = document.getElementById('header-board-name');
+let toDoTasksAmount = document.getElementById('toDoText');
+console.log(toDoTasksAmount);
+
 
 
 
@@ -18,31 +21,39 @@ async function main() {
     btns[i].addEventListener("click", () => {
       document.querySelector('.active')?.classList.remove('active');
       btns[i].classList.add('active')
-      
+
       selectedBoard = btns[i].innerText;
+      headerBoardName.innerText = selectedBoard;
+
       console.log("Button clicked:", selectedBoard);
       showTasks();
-      
+
     });
   }
 
   function showTasks() {
     columnDiv.innerHTML = `<div id="todo-head-div" class ="column-head-div"><span class="dot" id="todo-dot"></span><h4>TODO (4)</h4></div>`;
-    
+
     const filteredTasks = tasks.filter(task => task.board === selectedBoard);
     console.log(filteredTasks);
     console.log(selectedBoard);
 
     for (let i = 0; i < filteredTasks.length; i++) {
+     
       let taskDiv = document.createElement('div');
       taskDiv.innerText = filteredTasks[i].title; // changed from tasks[i].title to filteredTasks[i].title
       taskDiv.classList.add('task-div');
       columnDiv.appendChild(taskDiv);
 
-      
+     
     }
 
     
+
+
+
+
+
   }
 
 
